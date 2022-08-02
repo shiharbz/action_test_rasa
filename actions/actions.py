99f9ab8c6d3bd6aa49e27805cf3406a4ce5002ea
shiharbz <shiharudheen.k@bzanalytics.ai>
2022-08-02 16:01:06 +0530

    gpt action added

--- actions/actions.py
+++ actions/actions.py
@@ -17,101 +17,21 @@ from rasa_sdk.events import SlotSet
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-city_db = {
-    'brussels': 'Europe/Brussels', 
-    'zagreb': 'Europe/Zagreb',
-    'london': 'Europe/Dublin',
-    'lisbon': 'Europe/Lisbon',
-    'amsterdam': 'Europe/Amsterdam',
-    'seattle': 'US/Pacific'
-}
 
-class ActionHelloWorld(Action):
+class ActionGPT(Action):
 
     def name(self) -> Text:
-        return "action_tell_time"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-            
-        current_place = next(tracker.get_latest_entity_values("place"), None)
-        utc = arrow.utcnow()
-
-        if not current_place:
-            msg = f"It's {utc.format('HH:mm')} utc now. You can also give me a place."
-            dispatcher.utter_message(text=msg)
-            return []
-        
-        tz_string = city_db.get(current_place, None)
-        if not tz_string:
-            msg = f"It's I didn't recognize {current_place}. Is it spelled correctly?"
-            dispatcher.utter_message(text=msg)
-            return []
-                
-        msg = f"It's {utc.to(city_db[current_place]).format('HH:mm')} in {current_place} now."
-        dispatcher.utter_message(text=msg)
-        
-        return []
-
-class ActionRememberWhere(Action):
-
-    def name(self) -> Text:
-        return "action_remember_where"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        current_place = next(tracker.get_latest_entity_values("place"), None)
-        utc = arrow.utcnow()
-        
-        if not current_place:
-            msg = "I didn't get where you lived. Are you sure it's spelled correctly?"
-            dispatcher.utter_message(text=msg)
-            return []
-        
-        # tz_string = city_db.get(current_place, None)
-        # if not tz_string:
-        #     msg = f"I didn't recognize {current_place}. Is it spelled correctly?"
-        #     dispatcher.utter_message(text=msg)
-        #     return []
-        
-        msg = f"Sure thing! I'll remember that you live in {current_place}."
-        dispatcher.utter_message(text=msg)
-        
-        return [SlotSet("location", current_place)]
-
-class ActionCheerUp(Action):
-
-    def name(self) -> Text:
-        return "action_cheer_up"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-        r = requests.get('https://meme-api.herokuapp.com/gimme')
-        data = r.json()
-        msg = f"Here is something to cheer you up:"
-        dispatcher.utter_message(text=msg)
-        dispatcher.utter_message(image=data["url"])
-
-        return []
-
-
-class ActionSetName(Action):
-
-    def name(self) -> Text:
-        return "action_set_name"
+        return "action_gpt"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        name = next(tracker.get_latest_entity_values("name"), None)
-        print(name)
-        print("PERSON : ",[self.from_entity(entity="PERSON")])
-        print("name : ",[self.from_entity(entity="name")])
+        # txt=tracker.latest_message.text
+        # print("text : ",txt)
 
-        # return [SlotSet("logged_in", True), SlotSet("name", name)]  
-        return [SlotSet("logged_in", True)]  
+        current_state = tracker.current_state()
+        latest_message = current_state["latest_message"]["text"]
+        print("text : ", latest_message)
+
+        return[]
